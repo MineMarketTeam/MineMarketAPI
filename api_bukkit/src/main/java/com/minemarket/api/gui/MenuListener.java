a334e30feb7f1da5b273adac16cabfeb052c7a57
SAM_XPS <bsbcraftplays@gmail.com>
2017-11-05 21:03:27 -0200

    Creating a MenuListener

---
+++ api_bukkit/src/main/java/com/minemarket/api/gui/MenuListener.java
@@ -0,0 +1,53 @@
+package com.minemarket.api.gui;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+
+import com.minemarket.api.MineMarketBaseAPI;
+import com.minemarket.api.MineMarketBukkit;
+
+public class MenuListener implements Listener{
+	
+	@EventHandler
+	public void onClick(InventoryClickEvent event){
+		Inventory inv = event.getInventory();
+		if (inv.getHolder() instanceof MenuHolder){
+			event.setCancelled(true);
+			if (event.getWhoClicked() instanceof Player){
+				Player player = (Player) event.getWhoClicked();
+				MenuHolder holder = (MenuHolder) inv.getHolder();
+				MenuPage page = holder.getPage();
+				MenuItem clickedItem = page.getItem(event.getSlot());
+				ItemAction action = clickedItem == null ? ItemAction.DO_NOTHING : clickedItem.getAction();
+				if (action != null){
+					switch (action) {
+					case DO_NOTHING:
+						break;
+					case CLOSE_MENU:
+						event.getWhoClicked().closeInventory();
+						break;
+					case CHANGE_PAGE:
+						try {
+							MineMarketBukkit.getInstance().getPageManager().openPage(clickedItem.getActionData(), player);
+						} catch (NullPointerException e) {
+							player.closeInventory();
+							player.sendMessage(ChatColor.RED + "Página não encontrada.");
+						}
+						break;
+					case OPEN_URL:
+						player.sendMessage(ChatColor.GOLD + "Para prosseguir, acesse o link:");
+						player.sendMessage(clickedItem.getActionData());
+						break;
+					default:
+						break;
+					}
+				}
+			}
+		}
+	}
+
+}
